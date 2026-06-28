3c275a8c9ab50c6856b15cdbbe0d444b28ecdc41
Kamalkant Kumar <Kamalkantgupta06@gmail.com>
2026-06-28 19:10:03 +0530

    feat(infrastructure): add config server with secure environment variables

---
+++ spring-cloud-config/spring-cloud-config/src/main/java/com/example/spring_cloud_config/SpringCloudConfigApplication.java
@@ -0,0 +1,16 @@
+package com.example.spring_cloud_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+// 👇 CRITICAL: This annotation transforms the basic Spring Boot app into a Configuration Server
+@EnableConfigServer
+public class SpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigApplication.class, args);
+	}
+
+}
